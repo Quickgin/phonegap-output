8d726d4f3830c7ae535d57af3c16b6ce78b24f99
Quickgin <wanl2_15@uni.worc.ac.uk>
2017-11-16 15:14:17 +0000

    test

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,30 @@
 $(document).on("pagecreate","#pageone",function(){
-  $('#submitButton').on("click", function(){
-    submitText();
+  $('#Button').on("click", function(){
+    click();
   });            
 });            
 
+function random() {
+    return !Math.round(Math.random()); 
+}
 
-function submitText() {
+function click(){
+    var rand=random();
+    
+    if(rand)
+        navigator.notification.beep(1); 
+        //alert("hahaha");
+        
+    else
+        navigator.notification.beep(2); 
+        //alert("hehehe");
+}
+
+/*function submitText() {
 	var text = $('#textinput').val();
 	alert(text);
 }
 
 function storeValue(key, value) {
 	//add some code to store the key-value pair in persistant storage 
-}
+}*/
